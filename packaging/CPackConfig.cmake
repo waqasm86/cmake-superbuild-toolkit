a787dd221e600b1ef2d3beb9fc204d08da17f5a6
Muhammad Waqas <waqasm86@gmail.com>
2025-12-16 16:06:56 +0500

    Fix CI: logging OFF builds + CPack config order (Windows)

--- packaging/CPackConfig.cmake
+++ packaging/CPackConfig.cmake
@@ -1,9 +1,13 @@
-include(CPack)
-
 set(CPACK_PACKAGE_NAME "myapp-toolkit")
 set(CPACK_PACKAGE_VENDOR "waqasm86")
 set(CPACK_PACKAGE_CONTACT "waqasm86")
 set(CPACK_PACKAGE_VERSION ${PROJECT_VERSION})
 
-# Simple, cross-platform defaults
-set(CPACK_GENERATOR "TGZ;ZIP")
+# Prefer safe generators per-platform
+if(WIN32)
+  set(CPACK_GENERATOR "ZIP")
+else()
+  set(CPACK_GENERATOR "TGZ;ZIP")
+endif()
+
+include(CPack)
